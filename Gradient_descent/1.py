ab7fded93b22f9d473f0aca9e6e35898c9a95830
khanni1 <khanjanc.pro@gmail.com>
2026-08-03 20:06:18 +0530

    GDM update

--- Gradient_descent/1.py
+++ Gradient_descent/1.py
@@ -35,31 +35,70 @@ def GD(x,a,e):
         
         iter = iter + 1
         
-        print(f"{iter:<6} | {temp:<12.5f} | {df(x) :<12.5f} | {step_size :<12.5f} | {x :<12.5f} | {f(x) :<12.5f}")
+        print(f"{iter:<6} | {temp:<12.5f} | {df(temp) :<12.5f} | {step_size :<12.5f} | {x :<12.5f} | {f(x) :<12.5f}")
         
         xlst.append(iter)
         ylst.append(f(x))
         
     return xlst,ylst
+
+def GD_fixed_iter(x,a,iters):
+    
+    xlst = [] # no of iterations
+    ylst = [] # cost function
+    
+    # ini large value just for entering the loop
+    step_size = 100 
+    iter = 0
+    
+    print(f"{'Iter':<6} | {'x':<12} | {'df(x)':<12} | {'a*df(x)':<12} | {'x_new':<12} | {'f(x)':<12} |")
+    print("-" * 70)
+    
+    for i in range(0,iters,1):
+        
+        step_size = a*df(x)
+        
+        # just storing x's value before changing it further
+        temp = x 
+        
+        x = x - step_size
+        
+        iter = iter + 1
+        
+        print(f"{iter:<6} | {temp:<12.5f} | {df(temp) :<12.5f} | {step_size :<12.5f} | {x :<12.5f} | {f(x) :<12.5f}")
+        
+        xlst.append(iter)
+        ylst.append(f(x))
+        
+    return xlst,ylst
+
         
 # called the function
-x,y = GD(0,0.1,0.01)
-x1,y1 = GD(0,0.2,0.01)
-x2,y2 = GD(0,0.3,0.01)
-x3,y3 = GD(0,0.4,0.01)
-x4,y4 = GD(0,0.5,0.01)
+iters = 50
+a0 = 0.05
+a1 = 0.07
+a2 = 0.1
+a3 = 0.2
+a4 = 0.3
+a5 = 0.99
 
-x5,y5 = GD(0,0.99,0.01)
+x,y = GD_fixed_iter(0,0.05,iters)
+x1,y1 = GD_fixed_iter(0,0.07,iters)
+x2,y2 = GD_fixed_iter(0,0.1,iters)
+x3,y3 = GD_fixed_iter(0,0.2,iters)
+x4,y4 = GD_fixed_iter(0,0.3,iters)
+
+x5,y5 = GD_fixed_iter(0,0.99,100)
 
 
 
 # plotting
-plt.plot(x,y,marker='o',label = "a = 0.1")
-plt.plot(x1,y1,marker='o',label = "a = 0.2")
-plt.plot(x2,y2,marker='o',label = "a = 0.3")
-plt.plot(x3,y3,marker='o',label = "a = 0.4")
-plt.plot(x4,y4,marker='o',label = "a = 0.5")
-plt.plot(x5,y5,marker='o',label = "a = large")
+plt.plot(x,y,marker='o',label = f"a = {a0}")
+plt.plot(x1,y1,marker='o',label = f"a = {a1}")
+plt.plot(x2,y2,marker='o',label = f"a = {a2}")
+plt.plot(x3,y3,marker='o',label = f"a = {a3}")
+plt.plot(x4,y4,marker='o',label = f"a = {a4}")
+plt.plot(x5,y5,marker='o',label = f"a = {a5}")
 
 
 plt.legend()
